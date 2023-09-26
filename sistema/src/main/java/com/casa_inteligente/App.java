7b2c9ad321eb4151510fee108af15c365f317abf
Davi Cavalcante <93160711+DaviMacielCavalcante@users.noreply.github.com>
2023-09-26 19:40:31 -0300

    Criação do Projeto

---
+++ sistema/src/main/java/com/casa_inteligente/App.java
@@ -0,0 +1,13 @@
+package com.casa_inteligente;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
