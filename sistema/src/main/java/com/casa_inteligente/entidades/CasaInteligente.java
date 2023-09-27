d70f7e6924a66b2541ad89f40e99670a068e24bd
Davi Cavalcante <93160711+DaviMacielCavalcante@users.noreply.github.com>
2023-09-26 21:56:48 -0300

    Criação das Classes Principais

---
+++ sistema/src/main/java/com/casa_inteligente/entidades/CasaInteligente.java
@@ -0,0 +1,9 @@
+package com.casa_inteligente.entidades;
+
+import java.util.ArrayList;
+
+public class CasaInteligente {
+    
+
+    public ArrayList<Dispositivo> dispositivos = new ArrayList<>();
+}
